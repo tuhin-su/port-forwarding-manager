bb56b83a2b369d926b8ee3402032db64bac473a0
tuhin-su <tuhinsubhraghara29195@gmail.com>
2024-08-19 09:02:35 +0000

    Setserver Fix

--- server-setup.sh
+++ server-setup.sh
@@ -77,7 +77,7 @@ sudo chmod +x $SHELL_SCRIPT
 } | sudo tee -a $SSH_CONFIG
 
 echo -e "${GREEN}SSH configuration updated for $USERNAME.${RESET}"
-
+sudo cp -r $USER_HOME/.ssh/id_rsa
 # Restart SSH service to apply changes
 sudo systemctl restart ssh
 echo -e "${GREEN}SSH configuration updated and service restarted.${RESET}"
